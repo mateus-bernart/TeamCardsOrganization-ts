d3132a030e6d5b24ca02c4674f700e17014facf2
Andrei Ruan Steffens <andrei@movitech.com.br>
2024-12-20 13:17:02 -0300

    adiciona funcionalidade de exibição do formulário de colaboradores com botão de alternância

--- src/componentes/Botao/index.tsx
+++ src/componentes/Botao/index.tsx
@@ -3,10 +3,11 @@ import "./Botao.css";
 
 interface BotaoProps {
   children: ReactElement | string;
+  onClick?: () => void;
 }
 
 const Botao = (props: BotaoProps) => {
-  return <button className="botao">{props.children}</button>;
+  return <button className="botao" onClick={props.onClick}>{props.children}</button>;
 };
 
 export default Botao;
